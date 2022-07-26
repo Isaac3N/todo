6e3cbdc787888ae87d6c9663722940b5937332d0
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-26 11:23:42 +0100

    new updates

--- todo_drf/api/models.py
+++ todo_drf/api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
+class Todo(models.Model):
+    name = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
--- todo_drf/api/serializers.py
+++ todo_drf/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Todo
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Task
+        model = Todo
         fields = "__all__"
